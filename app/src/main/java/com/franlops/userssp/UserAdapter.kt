80bf5492622f4137805279eba77a571982271da5
Francisco Lopez <franlopbri@gmail.com>
2021-11-14 17:14:30 -0600

    Obtener nombre completo desde clase User

--- app/src/main/java/com/franlops/userssp/UserAdapter.kt
+++ app/src/main/java/com/franlops/userssp/UserAdapter.kt
@@ -26,7 +26,7 @@ class UserAdapter(private val users:List<User>) : RecyclerView.Adapter<UserAdapt
 
         with(holder){
             binding.tvOrder.text = (position + 1).toString()
-            binding.tvName.text = user.name
+            binding.tvName.text = user.getFullName()
             Glide.with(context)
                 .load(user.url)
                 .diskCacheStrategy(DiskCacheStrategy.ALL)
