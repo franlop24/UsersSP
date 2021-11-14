b782d3ec1a89a981402fe9cfe182566d6c778188
Francisco Lopez <franlopbri@gmail.com>
2021-11-14 17:03:08 -0600

    Cargar imagen con Glide

--- app/src/main/java/com/franlops/userssp/MainActivity.kt
+++ app/src/main/java/com/franlops/userssp/MainActivity.kt
@@ -30,10 +30,30 @@ class MainActivity : AppCompatActivity() {
     private fun getUsers(): MutableList<User> {
         val users = mutableListOf<User>()
 
-        val francisco = User(1, "Francisco", "Lopez", "")
-        val jose = User(2, "Jose", "Perez", "")
+        val francisco = User(1, "Francisco", "Lopez", "https://avatars.githubusercontent.com/u/39685222?s=400&u=a8044460a19cd7e2f5198d170db4963080d63e34&v=4")
+        val jose = User(2, "Jose", "Perez", "https://avatars.githubusercontent.com/u/51543942?v=4")
+        val pedro = User(3, "Pedro", "Gomez", "")
+        val andrea = User(4, "Andrea", "Ramirez", "")
+        val jessica = User(5, "Jessica", "Lopez", "")
+        val fernanda = User(6, "Fernanda", "Contreras", "")
         users.add(francisco)
         users.add(jose)
+        users.add(pedro)
+        users.add(andrea)
+        users.add(jessica)
+        users.add(fernanda)
+        users.add(francisco)
+        users.add(jose)
+        users.add(pedro)
+        users.add(andrea)
+        users.add(jessica)
+        users.add(fernanda)
+        users.add(francisco)
+        users.add(jose)
+        users.add(pedro)
+        users.add(andrea)
+        users.add(jessica)
+        users.add(fernanda)
 
         return users
     }
